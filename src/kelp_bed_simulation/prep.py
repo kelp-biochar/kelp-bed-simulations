adfc65db15887650da4cb63c50101f2ba7a45c4e
Jeremy Nelson <jermnelson@gmail.com>
2024-04-29 16:55:04 -0700

    Ran through first scenario, updates code for simulation

--- src/kelp_bed_simulation/prep.py
+++ src/kelp_bed_simulation/prep.py
@@ -12,6 +12,8 @@ from kelp_drone.llms import (
     Gemini
 )
 
+from kelp_drone.somu import SpeciesOccurrenceManagementUnit as SOMU
+
 httpx_client = httpx.AsyncClient()
 
 chatgpt = ChatGPT(
@@ -32,3 +34,10 @@ gemini = Gemini(
     key=os.environ.get("GOOGLE_GEMINI_KEY"),
     post_function=httpx_client.post
 )
+
+def add_drone_somus(drone, number=3):
+    """Adds Species Occurrence Management Units to a Drone"""
+    for i in range(1, number+1):
+        somu = SOMU(name=f"{drone.name} {i}")
+        drone.add_somu(somu)
+
--- src/kelp_bed_simulation/simulation.py
+++ src/kelp_bed_simulation/simulation.py
@@ -1,5 +1,6 @@
 __author__ = "Jeremy Nelson"
 
+import datetime
 import json
 import pathlib
 
@@ -7,9 +8,22 @@ from attrs import define, field
 
 from kelp_drone.drone import Drone
 
+
+@define
+class Place:
+    latitude: float
+    longitude: float
+
+@define
+class Scenario:
+    place: Place
+
 @define
 class Simulation:
+    scenario: Scenario
+    created_on: datetime = field(default=datetime.datetime.now(datetime.UTC)) 
     epochs: int = field(default=5)
     history: list = field(factory=[])
+    
 
 
